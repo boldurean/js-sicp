c999762736dd59c48a55d5c6897e8223c69bbde4
vsbdn <v.boldurean@gmail.com>
2020-10-30 19:38:20 +0000

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# GeneratorHTML--SICP
+# JS Sequences: HTML
 
 ![Node CI](https://github.com/vsbdn/GeneratorHTML--SICP/workflows/Node%20CI/badge.svg)
--- src/js/sequence representation.js
+++ src/js/sequence representation.js
@@ -37,3 +37,35 @@ const concat = (list1, list2) => {
 };
 
 console.log(listToString(concat(l(3, 5, 8, 4), l(2, 9, 7))));
+
+
+/*
+
+export const has = (list, element) => {
+  if (isEmpty(list)) {
+    return false;
+  }
+  if (head(list) === element) {
+    return true;
+  }
+
+  return has(tail(list), element);
+};
+
+export const reverse = (list) => {
+  const iter = (items, acc) => (
+    isEmpty(items) ? acc : iter(tail(items), consList(head(items), acc))
+  );
+
+  return iter(list, l());
+};
+
+export const concat = (list1, list2) => {
+  if (isEmpty(list1)) {
+    return list2;
+  }
+
+  return consList(head(list1), concat(tail(list1), list2));
+};
+
+*/
