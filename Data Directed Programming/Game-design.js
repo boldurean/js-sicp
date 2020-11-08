7d0bc21336d0571ac6ea9bd7902af3b269b43821
vsbdn <v.boldurean@gmail.com>
2020-11-08 20:54:15 +0000

    Added game design file

---
+++ Data Directed Programming/Game-design.js
@@ -0,0 +1,18 @@
+// import pairs from '@hexlet/pairs';
+// import pairsData from '@hexlet/pairs-data';
+// const { cons, car, cdr, toString: pairToString } = pairs; // eslint-disable-line
+// eslint-disable-next-line max-len
+// const { cons: consList, l, random, head, reverse, toString: listToString } = pairsData; // eslint-disable-line
+//
+// const run = (player1, player2, cards) => {
+//   const iter = (health1, name1, health2, name2, order, log) => {
+//     // BEGIN (write your solution here)
+//
+//     // END
+//   };
+//
+//   const startHealth = 10;
+//   const logItem = cons(cons(startHealth, startHealth), 'Начинаем бой!');
+//   return reverse(iter(startHealth, player1, startHealth, player2, 1, l(logItem)));
+// };
+// export default (cards) => (name1, name2) => run(name1, name2, cards);
