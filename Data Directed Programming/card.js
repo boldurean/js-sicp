99813b50604d2cfa3a03d43d239c8e247ede279e
vsbdn <v.boldurean@gmail.com>
2020-11-29 11:47:22 +0000

    Deleted unused files/code.

--- Data Directed Programming/card.js
+++
@@ -1,6 +0,0 @@
-import { contents } from '@hexlet/tagged-types';
-import { getMethod } from './generic.js';
-
-export const getName = (self) => getMethod(self, 'getName')(contents(self));
-
-export const damage = (self, health) => getMethod(self, 'damage')(contents(self), health);
