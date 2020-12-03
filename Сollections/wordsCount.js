acde5aed5fe6f004bc66b379ced3b33ebaa227e0
vsbdn <v.boldurean@gmail.com>
2020-12-03 20:08:40 +0000

    Added example arrs

--- Сollections/wordsCount.js
+++ Сollections/wordsCount.js
@@ -1,7 +1,12 @@
-export default (words, stopWords) => words
+const wordsCount = (words, stopWords) => words
   .map((word) => word.toLowerCase())
   .filter((word) => !stopWords.includes(word))
   .reduce((acc, word) => {
     const value = acc.get(word) ?? 0;
     return acc.set(word, value + 1);
   }, new Map());
+
+const stopWords = ['and', 'or', 'a', 'the', ''];
+const words = ['HellO', 'h', 'And', 'heLlo', '', 'AND', 'DOG', 'oR', 'cat', 'HELLO', 'caT'];
+
+console.log(wordsCount(words, stopWords));
